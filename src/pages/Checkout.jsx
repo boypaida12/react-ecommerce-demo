6ecc50b6a463e77f6bdd068d89708eafa60f635e
boypaida12 <baddysackey5@gmail.com>
2023-08-12 09:08:04 +0000

    paystack api accept payment practice

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import {
+  Box,
   Button,
   FormControl,
   FormErrorMessage,
@@ -13,8 +14,9 @@ import { Field, Form, Formik } from "formik";
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import ProductImage from "../assets/image-product-1.jpg"
+import ProductImage from "../assets/image-product-1.jpg";
 import Navigation from "../components/Navigation";
+import { PaystackButton } from "react-paystack";
 
 function Checkout() {
   const state = useSelector((state) => {
@@ -27,15 +29,38 @@ function Checkout() {
 
   const total = subTotal - discount;
 
-  function validateCardNumber(value) {
-    let error;
-    if (!value) {
-      error = "Card number required";
-    } else if (!/^\d{16,22}$/.test(value)) {
-      error = "Card number must be 16 to 22 digits";
-    }
-    return error;
+  const publicKey = "pk_test_afa72ca74eb957e5423f4e78f7b8d1a72f74e854";
+  const amount = total * 100; // Remember, set in kobo!
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const componentProps = {
+    email,
+    amount,
+    currency: "GHS",
+    metadata: {
+      name,
+      phone,
+    },
+    publicKey,
+    text: "Pay Now",
+    onSuccess: () =>
+      alert("Thanks for doing business with us! Come back soon!!"),
+    onClose: () => alert("Wait! Don't leave :("),
   }
+  
+  
+
+  // function validateCardNumber(value) {
+  //   let error;
+  //   if (!value) {
+  //     error = "Card number required";
+  //   } else if (!/^\d{16,22}$/.test(value)) {
+  //     error = "Card number must be 16 to 22 digits";
+  //   }
+  //   return error;
+  // }
 
   function validateEmail(value) {
     let error;
@@ -47,76 +72,105 @@ function Checkout() {
     return error;
   }
 
-  function validateCVC(value) {
+  function validateName(value) {
     let error;
     if (!value) {
-      error = "CVC is required";
-    } else if (!/^\d{3,4}$/.test(value)) {
-      error = "CVC must be a 3 or 4-digit number";
-    }
+      error = "Full name is required";
+    } 
     return error;
   }
 
-  function validateExpiryDate(value) {
-    let error;
-    if (!value) {
-      error = "Expiry date is required";
-    } else if (!/^\d{2}\/\d{2}$/.test(value)) {
-      error = "Expiry date must be in the format MM/YY";
-    } else {
-      const [month, year] = value.split("/");
-      const currentDate = new Date();
-      const currentYear = currentDate.getFullYear() % 100;
-      const currentMonth = currentDate.getMonth() + 1;
+  // function validateCVC(value) {
+  //   let error;
+  //   if (!value) {
+  //     error = "CVC is required";
+  //   } else if (!/^\d{3,4}$/.test(value)) {
+  //     error = "CVC must be a 3 or 4-digit number";
+  //   }
+  //   return error;
+  // }
 
-      if (parseInt(year, 10) < currentYear) {
-        error = "Expiry date has passed";
-      } else if (
-        parseInt(year, 10) === currentYear &&
-        parseInt(month, 10) < currentMonth
-      ) {
-        error = "Expiry date has passed";
-      }
-    }
-    return error;
-  }
+  // function validateExpiryDate(value) {
+  //   let error;
+  //   if (!value) {
+  //     error = "Expiry date is required";
+  //   } else if (!/^\d{2}\/\d{2}$/.test(value)) {
+  //     error = "Expiry date must be in the format MM/YY";
+  //   } else {
+  //     const [month, year] = value.split("/");
+  //     const currentDate = new Date();
+  //     const currentYear = currentDate.getFullYear() % 100;
+  //     const currentMonth = currentDate.getMonth() + 1;
+
+  //     if (parseInt(year, 10) < currentYear) {
+  //       error = "Expiry date has passed";
+  //     } else if (
+  //       parseInt(year, 10) === currentYear &&
+  //       parseInt(month, 10) < currentMonth
+  //     ) {
+  //       error = "Expiry date has passed";
+  //     }
+  //   }
+  //   return error;
+  // }
 
   return (
     <>
-      <Navigation/>
+      <Navigation />
       <Container
         className="bg-body-tertiary py-5 py-lg-0 d-flex flex-column justify-content-center align-items-center min-vh-100"
         fluid
       >
         <Grid
-          templateColumns={{base: "1fr", md:"50% 45%"}}
+          templateColumns={{ base: "1fr", md: "50% 45%" }}
           gap={10}
           bgColor={"white"}
           boxShadow={"lg"}
           maxW={1080}
           overflow={"hidden"}
-          p={{base:5,lg:10}}
+          p={{ base: 5, lg: 10 }}
           rounded={"2xl"}
-
         >
           <GridItem>
             <p className="fw-bold text-grey-50">Payment details</p>
             <Formik initialValues={{ name: "" }}>
               {(props) => (
-                <Form autoComplete="off">
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}
+                        onChange={(e) => setName(e.target.value)}
+                        my={5}
+                      >
+                        <FormLabel>Full name</FormLabel>
+                        <Input
+                          {...field}
+                          placeholder="Kabutey Kwashie"
+                          _placeholder={{ fontSize: "14px" }}
+                        />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
                   <Field name="email" validate={validateEmail}>
                     {({ field, form }) => (
                       <FormControl
                         isInvalid={form.errors.email && form.touched.email}
                         my={5}
+                        onChange={(e) => setEmail(e.target.value)}
                       >
                         <FormLabel>Email address</FormLabel>
-                        <Input {...field} placeholder="example@email.com" _placeholder={{fontSize:"14px"}}/>
-                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                        <Input
+                          {...field}
+                          placeholder="example@email.com"
+                          _placeholder={{ fontSize: "14px" }}
+                        />
+                        {/* <FormErrorMessage>{form.errors.email}</FormErrorMessage> */}
                       </FormControl>
                     )}
                   </Field>
-                  <Field name="number" validate={validateCardNumber}>
+                  {/* <Field name="number" validate={validateCardNumber}>
                     {({ field, form }) => (
                       <FormControl
                         isInvalid={form.errors.number && form.touched.number}
@@ -134,8 +188,8 @@ function Checkout() {
                         </FormErrorMessage>
                       </FormControl>
                     )}
-                  </Field>
-                  <Grid templateColumns={{base: "1fr", lg:"repeat(2, 1fr)"}} gap={5} my={5}>
+                  </Field> */}
+                  {/* <Grid templateColumns={{base: "1fr", lg:"repeat(2, 1fr)"}} gap={5} my={5}>
                     <Field name="date" validate={validateExpiryDate}>
                       {({ field, form }) => (
                         <FormControl
@@ -170,7 +224,7 @@ function Checkout() {
                         </FormControl>
                       )}
                     </Field>
-                  </Grid>
+                  </Grid> */}
                   <div className="d-flex justify-content-between mb-3">
                     <span>Subtotal</span>
                     <small className="fw-bold">${subTotal}</small>
@@ -184,26 +238,13 @@ function Checkout() {
                     <span>Total</span>
                     <span className="fw-bold">${total}</span>
                   </div>
-                  <Button
-                    mt={12}
-                    backgroundColor={"hsl(26, 100%, 55%)"}
-                    _hover={{
-                      backgroundColor: "hsl(26, 100%, 55%)",
-                      boxShadow: "lg",
-                    }}
-                    color={"whiteAlpha.900"}
-                    width={{base: "100%"}}
-                    marginInline={{base: "auto"}}
-                    type="submit"
-                  >
-                    Pay Now
-                  </Button>
+                    <PaystackButton className="paystack-button" {...componentProps} />
                 </Form>
               )}
             </Formik>
           </GridItem>
           <GridItem alignSelf={"center"} order={"-1"}>
-            <Image src={ProductImage} alt="/" rounded={"lg"}/>
+            <Image src={ProductImage} alt="/" rounded={"lg"} />
             {/* <MobileScreenSlide/> */}
           </GridItem>
         </Grid>
